6dd7052453f8982f1fd8387aa405d0f9d175b92b
oded-navon <oded.navon@gmail.com>
2017-06-16 15:58:04 +0300

    First commit
    First question done

---
+++ ComputeProjective.m
@@ -0,0 +1,12 @@
+function [ TransformedIm ] = ComputeProjective(Im, H )
+%ComputeProjective computes a projective transformation of the input image.
+%   Detailed explanation goes here
+img = imread(Im);
+tform = projective2d(H);
+TransformedIm = imwarp(img,tform);
+
+figure;
+imshow(TransformedIm);
+imwrite(TransformedIm, 'cityOutput.jpg', 'jpg');
+end
+
